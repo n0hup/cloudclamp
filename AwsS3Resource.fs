4207e4411766c9bce955207553c7f121d1ef1349
Istvan <istvan@lambdainsight.com>
2020-06-06 13:31:04 +0200

    New types

---
+++ AwsS3Resource.fs
@@ -0,0 +1,190 @@
+namespace CloudClamp
+
+// external
+
+// internal
+
+module AwsS3Resource =
+
+  //
+  // BucketAccelerateConfiguration
+  //  
+
+  type BucketAccelerateConfiguration = {
+    Name : string
+  }
+  
+  //
+  // BucketAcl
+  //
+
+  type Owner = {
+    DisplayName : string
+    ID          : string
+  }
+
+  type Permission = FullControl | Read 
+
+  type GranteeType = CanonicalUser | Group
+
+  type Grantee = {
+    DisplayName : string
+    ID          : string
+    GranteeType : GranteeType
+  }
+
+  type Grant = {
+    Permission : Permission
+    Grantee : Grantee
+  }
+
+  type BucketAcl = {
+    Owner   : Owner
+    Grants  : List<Grant>
+  }
+
+  //
+  // BucketAnalyticsConfiguration
+  //
+
+  type BucketAnalyticsConfiguration = {
+    Name : string
+  }
+
+  //
+  // BucketCors
+  //
+
+  type BucketCors = {
+    Name : string
+  }
+
+  //
+  // BucketEncryption
+  //
+
+  type BucketEncryption = {
+    Name : string
+  }
+
+  //
+  // BucketInventoryConfiguration
+  //
+
+  type BucketInventoryConfiguration = {
+    Name : string
+  }
+
+  //
+  // BucketLifecycleConfiguration
+  //
+
+  type BucketLifecycleConfiguration = {
+    Name : string
+  }
+  
+  //
+  // BucketLocation
+  //
+
+  type BucketLocation = {
+    LocationConstraint : string
+  }
+
+  //
+  // BucketLogging
+  // 
+
+  type BucketLogging = {
+    TargetBucket : string
+    TargetPrefix : string
+  }
+
+  //
+  // BucketMetricsConfiguration
+  // 
+
+  type BucketMetricsConfiguration = {
+    Name : string
+  }
+
+  //
+  // BucketNotificationConfiguration
+  //  
+  
+  type BucketNotificationConfiguration = {
+    Name : string
+  }
+
+  //
+  // BucketPolicy
+  //
+
+  type BucketPolicy = {
+    Name : string
+  }
+
+  //
+  // BucketPolicyStatus
+  //
+
+  type BucketPolicyStatus = {
+    Name : string
+  }
+
+  //
+  // BucketReplication
+  //
+
+  type BucketReplication = {
+    Name : string
+  }
+
+  //
+  // BucketRequestPayment
+  //
+
+  type BucketRequestPayment = {
+    Name : string
+  }
+
+  //
+  // BucketTagging
+  //
+
+  type Tag = {
+    Key   : string
+    Value : string
+  }
+
+  type BucketTagging = {
+    TagSet : List<Tag>
+  }
+
+  //
+  // BucketVersioning
+  //
+
+  type BucketVersioning = {
+    Name : string
+  }
+
+  //
+  // BucketWebsite
+  //
+
+  type Website = 
+    | Documents of IndexDocument : string * ErrorDocument : string
+    | Redirect of RedirectAllRequestsTo : string
+
+  type BucketWebsite = {
+    Website : Website
+  }
+
+  //
+  // BucketPublicAccessBlock
+  //
+
+  type BucketPublicAccessBlock = {
+    Name : string
+  }
---
+++ BlogStack.fs
@@ -0,0 +1,31 @@
+namespace CloudClamp
+
+// external
+
+// internal
+
+
+module BlogStack =
+  
+  type DnsResource = AwsRoute53Resource 
+
+  type CertificateResource = AwsAcmResource 
+
+  type BucketResource = {
+    //    mandatory
+    Name      : string
+    Location  : AwsS3Resource.BucketLocation
+    Acl       : AwsS3Resource.BucketAcl
+    Tagging   : AwsS3Resource.BucketTagging
+    //    optional
+    Policy    : Option<AwsS3Resource.BucketPolicy>
+    Logging   : Option<AwsS3Resource.BucketLogging>
+    Website   : Option<AwsS3Resource.BucketWebsite>
+
+  } 
+
+  type Stack = {
+    Dns         : List<DnsResource>
+    Certificate : List<CertificateResource>
+    Bucket      : List<BucketResource>
+  }
