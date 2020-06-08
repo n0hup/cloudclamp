cc92c21d2a619b61f852fcf8f50a555aad382666
Istvan <istvan@lambdainsight.com>
2020-06-08 22:46:28 +0200

    Json Decoderz

--- Blog.fs
+++ Blog.fs
@@ -1,20 +1,16 @@
 namespace CloudClamp
 
 // external
+// open Newtonsoft.Json
+open Thoth.Json
+
 // internal
 open AwsRoute53Resource
 open Logging
 open Config
-
-
+open System.IO
 
 module Blog =
-
-  type Blog = {
-    Dns         : List<AwsRoute53Resource.DnsResource>
-    // Certificate : List<AwsAcmResource.CertificateResource>
-    // Bucket      : List<AwsS3Resource.BucketResource>
-  }
 
   let loggerBlog = Logger.CreateLogger "Website" loggingConfig.LogLevel
 
@@ -65,74 +61,26 @@ module Blog =
   let dnsResource =
     { Name = "l1x.be"; HostedZoneId = None; ResourceRecordSets = resourceRecordSets }
 
+  type Blog = {
+    DnsZones  : List<AwsRoute53Resource.DnsResource>
+  }
 
-  let nemtudmmi (dnsResource:DnsResource) =
-    """
-        - HostedZoneId: /hostedzone/Z04374393O7HMC10LT1Q2
-      Name: l1x.be.
-      ResourceRecordSets:
-        - Name: l1x.be.
-          ResourceRecords:
-            - Value: ns-583.awsdns-08.net.
-            - Value: ns-504.awsdns-63.com.
-            - Value: ns-1708.awsdns-21.co.uk.
-            - Value: ns-1176.awsdns-19.org.
-          TTL: 172800
-          Type: NS
-        - Name: l1x.be.
-          ResourceRecords:
-            - Value: ns-583.awsdns-08.net. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400
-          TTL: 900
-          Type: SOA
-        - AliasTarget:
-            DNSName: dbrgct5gwrbsd.cloudfront.net.
-            EvaluateTargetHealth: false
-            HostedZoneId: Z2FDTNDATAQYW2
-          Name: dev.l1x.be.
-          Type: A
-        - AliasTarget:
-            DNSName: dbrgct5gwrbsd.cloudfront.net.
-            EvaluateTargetHealth: false
-            HostedZoneId: Z2FDTNDATAQYW2
-          Name: dev.l1x.be.
-          Type: AAAA
-        - Name: _0a772de2cfcacc285d64f76d53afb931.dev.l1x.be.
-          ResourceRecords:
-            - Value: _031a48dd8cb38a7723ba15eeef0ae2b2.tfmgdnztqk.acm-validations.aws.
-          TTL: 300
-          Type: CNAME
-    """
+  let blog : Blog =
+    {
+      DnsZones = [dnsResource]
+    }
+
+  // type BlogState =
+  //   JsonProvider<"state/prod/blog.json">
+
+  // let blogState (stage:string) =
+  //   BlogState.Parse(File.ReadAllText(sprintf "state/%s/blog.json" stage))
 
   let executeCommand command stage =
     let log = sprintf "command: %s stage: %s" command stage
+    // let options = JsonSerializerOptions()
+    // options.Converters.Add(JsonFSharpConverter())
+    // let soap = JsonSerializer.Serialize(soa, options)
     loggerBlog.LogInfo log
+    // loggerBlog.LogInfo (sprintf "%A" soap)
 
-
-
-
-// "module": "module.aws-acm-certs-trck-dev",
-// "mode": "managed",
-// "type": "aws_acm_certificate",
-// "name": "cert",
-// "provider": "provider.aws.us-east-1",
-// "instances": [
-//   {
-//     "schema_version": 0,
-//     "attributes": {
-//       "arn": "arn:aws:acm:us-east-1:651831719661:certificate/7112ec11-b23b-42df-87f9-08c657666c71",
-//       "certificate_authority_arn": "",
-//       "certificate_body": null,
-//       "certificate_chain": null,
-//       "domain_name": "dev-trck.lambdainsight.com",
-//       "domain_validation_options": [
-//         {
-//           "domain_name": "dev-trck.lambdainsight.com",
-//           "resource_record_name": "_90f50843c005bddd7414f9abc8e8093b.dev-trck.lambdainsight.com.",
-//           "resource_record_type": "CNAME",
-//           "resource_record_value": "_059fbaf9e496135ae14993eb8800bd56.mzlfeqexyx.acm-validations.aws."
-//         }
-//       ],
-//       "id": "arn:aws:acm:us-east-1:651831719661:certificate/7112ec11-b23b-42df-87f9-08c657666c71",
-//       "options": [
-//         {
-//           "certificate_transparency_logging_preference": "ENABLED"
